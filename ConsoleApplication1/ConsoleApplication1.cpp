c69a0b099bba174c3c82a2b35eca5a43ee9ddae3
nikit <nikita.vorontsov.1989@mail.ru>
2024-05-05 12:57:43 +0300

    new all

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -1,7 +1,29 @@
 ﻿#include <iostream>
+#include <stack>
 using namespace std;
+/*
+Разработать функцию, которая удаляет из стека первый
+отрицательный элемент, если такой есть.
+*/
+
+void Function() {
+
+}
 
 int main() {
+	setlocale(LC_ALL, "Russian");
 
-	cout << "Hello World!" << endl;
+	stack<int> func_element;
+	stack<int> Perezapis;
+	int i = 0;
+	cout << "Сколько чисел вы хотите ввести:";
+	cin >> i;
+	while (i < 5) {
+		cout << "Введите число для стека: ";
+		int Number;
+		cin >> Number;
+		func_element.push(Number);
+	}
+	cout << func_element.size();
+	return 0;
 }
